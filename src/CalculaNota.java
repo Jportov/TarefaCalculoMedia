969730a40a1378f6595a40ac3705c77d1dcfa6ec
Jportov <joaovportog@gmail.com>
2024-09-02 08:33:34 -0300

    2 commit

---
+++ src/CalculaNota.java
@@ -0,0 +1,18 @@
+public class CalculaNota {
+    public static void main(String[] args) {
+
+
+        int nota1 = 5;
+        int nota2 = 5;
+        int nota3 = 5;
+        int nota4 = 5;
+
+        int media = (nota1+ nota2 + nota3 + nota4) / 4 ;
+
+
+
+
+
+        System.out.println(media);
+    }
+}
